fc84684bf3d2b5ec6d7db4288a4ce1247c32eccd
陈柱锋 <2414802324@qq.com>
2024-02-07 18:06:04 +0800

    .

--- deployment/build-image.bat
+++ deployment/build-image.bat
@@ -13,9 +13,9 @@ for /f "tokens=1,* delims==" %%a in (.env) do (
     set "BUILD_ARGS=!BUILD_ARGS! --build-arg %%a=%%b"
 )
 
-docker build %BUILD_ARGS% -t kcdesign-doc-versioning-service:%VERSION_TAG% -f ./Dockerfile ../
-docker tag kcdesign-doc-versioning-service:%VERSION_TAG% docker-registry.protodesign.cn:35000/kcdesign-doc-versioning-service:%VERSION_TAG%
+docker build %BUILD_ARGS% -t doc-versioning-service:%VERSION_TAG% -f ./Dockerfile ../
+docker tag doc-versioning-service:%VERSION_TAG% docker-registry.protodesign.cn:35000/doc-versioning-service:%VERSION_TAG%
 docker login docker-registry.protodesign.cn:35000 -u kcai -p kcai1212
-docker push docker-registry.protodesign.cn:35000/kcdesign-doc-versioning-service:%VERSION_TAG%
+docker push docker-registry.protodesign.cn:35000/doc-versioning-service:%VERSION_TAG%
 
 endlocal

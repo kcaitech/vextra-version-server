21dceee2b945137bef386c4a82bda9c83e0cff8b
陈柱锋 <2414802324@qq.com>
2024-04-19 12:13:20 +0800

    pathop

--- pal/pathop.ts
+++ pal/pathop.ts
@@ -111,11 +111,12 @@ interface PathKitPath {
 
 
 
+const ServerPort = 10040 // 10040
 let _ck: PathKit;
 export async function init() {
     if (_ck) return;
     _ck = await PathKitInit({
-        locateFile: (file: string) => file
+        locateFile: (file: string) => `http://localhost:${ServerPort}/` + file
     })
 }
 

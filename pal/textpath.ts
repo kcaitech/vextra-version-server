f011d024a117f687516d959375876bc64e4c3949
Ruiqiang Zhang <zhangruiqiang@kcaitech.com>
2024-05-13 11:27:21 +0800

    同步修改文本绘制的对齐方式

--- pal/textpath.ts
+++ pal/textpath.ts
@@ -1,25 +1,29 @@
-import {Canvas} from "skia-canvas"
+import { Canvas } from "skia-canvas"
+import { Path } from "@kcdesign/data";
 
 const textPathCache: Map<string, string> = new Map()
-export function getTextPath(font: string, fontSize: number, charCode: number) {
+export function getTextPath(font: string, fontSize: number, italic: boolean, weight: number, charCode: number) {
     fontSize = Math.round(fontSize)
 
-    const cacheId = font + "#" + fontSize + "#" + charCode
+    const cacheId = font + "#" + fontSize + "#" + charCode + (italic ? '#i' : '') + '#' + weight;
     let path = textPathCache.get(cacheId)
     if (path) return path;
 
-    const width = fontSize
-    const height = Math.round(fontSize * 1.1)
+    const size = Math.round(fontSize * 1.25);
+    // const width = fontSize
+    // const height = Math.round(fontSize * 1.1)
 
-    const canvas = new Canvas(width, height)
+    const canvas = new Canvas(size, size)
     const ctx = canvas.getContext("2d")
     ctx.imageSmoothingEnabled = false
 
     ctx.font = `${fontSize}px ${font}`
     ctx.fillStyle = "black"
     ctx.textAlign = "left"
-    ctx.textBaseline = "top"
-    ctx.fillText(String.fromCharCode(charCode), 0, 0)
+    ctx.textBaseline = "alphabetic"
+
+    const offsetX = Math.round(fontSize * 0.1); // j会左边截断
+    ctx.fillText(String.fromCharCode(charCode), offsetX, fontSize)
 
     const svgData = canvas.toBufferSync("svg", { outline: true })
 
@@ -27,6 +31,12 @@ export function getTextPath(font: string, fontSize: number, charCode: number) {
     const regRes = pathReg.exec(svgData.toString())
     if (!regRes) return "";
     path = regRes[1]
-    textPathCache.set(cacheId, path)
-    return path
+
+    // 还要进行偏移下
+    const _path = new Path(path);
+    _path.translate(-offsetX, -fontSize);
+
+    const ret = _path.toString();
+    textPathCache.set(cacheId, ret)
+    return ret
 }

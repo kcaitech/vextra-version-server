2f87a76f372e13af9d2705e65f1ddcdf94b48c66
Jfeng <2414802324@qq.com>
2024-05-30 11:01:01 +0800

    指定输出为 CommonJS 模块

--- webpack.config.js
+++ webpack.config.js
@@ -30,10 +30,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
-        libraryTarget: "module",  // 指定输出为 ES 模块
-        chunkFormat: "module",    // 指定 chunk 格式为 module
-    },
-    experiments: {
-        outputModule: true,  // 启用 ES 模块输出
+        libraryTarget: "commonjs",  // 指定输出为 CommonJS 模块
     },
 };

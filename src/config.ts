dc35690dc10aeee52d7fc980440b61d6680f9f3d
zhangruiqiang <zhangruiqiang@kcaitech.com>
2024-10-25 09:29:15 +0800

    config

--- src/config.ts
+++ src/config.ts
@@ -35,20 +35,20 @@ type Config = {
 
 // sae的变量由env传递
 // k8s可由configmap或者secret进行文件挂载
-
+import yaml from "js-yaml"
 import fs from "fs"
 import example from "../config/config.json"
 function compile_check(_: Config) { }
 compile_check(example)
 
 let config: Config = example;
-const configfile = 'config/config.json'
+const configfile = 'config/config.yaml'
 if (process.env.kcconfig) {
     config = Object.assign(config, JSON.parse(process.env.kcconfig));
 }
 else if (fs.existsSync(configfile)) {
     const cf = fs.readFileSync(configfile);
-    config = Object.assign(config, JSON.parse(cf.toString()));
+    config = Object.assign(config, yaml.load(cf.toString()));
 }
 
 export default config;
